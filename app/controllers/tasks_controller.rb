e6aaf2bb571a3acf67427db939dc5e009cc499ae
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-07-01 06:05:53 +0000

    削除機能の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks =Task.all
 
@@ -19,15 +20,15 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_path, notice: "更新しました！"
     else
@@ -35,10 +36,21 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+      @task.destroy
+      redirect_to tasks_path, notice: "削除しました!"
+  end
+
+
+
 
 
   private
   def task_params
     params.require(:task).permit(:name, :detail)
+  end
+
+  def set_task
+   @task = Task.find(params[:id])
   end
 end
