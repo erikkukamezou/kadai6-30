532c776afff39ed40661bdf7daffd7df98fe18b1
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-07-01 05:34:36 +0000

    エラーメッセージ機能の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,12 +9,30 @@ class TasksController < ApplicationController
   end
 
   def create
-  Task.create(task_params)
-  redirect_to tasks_path
+  # Task.create(task_params)
+    @task =Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "新規作成しました！"
+    else
+       render :new
+    end
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "更新しました！"
+    else
+      render :edit
+    end
   end
 
 
