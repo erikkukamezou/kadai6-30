b0bfe80ff75b167c313344578185f1474e3a796d
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-07-01 15:02:33 +0000

    rspecを記載

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -10,11 +10,15 @@ describe 'タスクモデル機能', type: :model do
 　　context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
         # ここに内容を記載する
+        task =Task.new(name:'失敗テスト', detail:'')
+        expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
         # ここに内容を記載する
+        task = Task.new(name: '失敗テスト', detail: '失敗テスト')
+        expect(tack) to pass
       end
     end
   end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -4,18 +4,54 @@ describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, title: 'task')
+        task = FactoryBot.create(:task, name: 'task')
         # タスク一覧ページに遷移
         visit tasks_path
-        # ページ遷移直後でデバッグ
-        binding.irb
-        # タスク一覧ページに遷移できているかを確認
-        current_path
-        # タスクがデータベースに作成されているかを確認
-        Task.count
-        # 表示するHTMLにタスク情報が入っているかを確認
-        page.html
+        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
+        # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
         expect(page).to have_content 'task'
+        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
+      end
+    end
+  end
+end
+
+
+require 'rails_helper'
+describe 'タスク管理機能', type: :system do
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+      # 1. new_task_pathに遷移する（新規作成ページに遷移する）
+      # ここにnew_task_pathにvisitする処理を書く
+        visit new_task_path
+      # 2. 新規登録内容を入力する
+      #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
+      # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+      fill_in 'task[name]'
+      # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+      fill_in 'task[detail]'
+      # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
+      # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+      click_button 'Register'
+      # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
+      # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
+      # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+      expect(page).to have_content date
+      end
+    end
+  end
+end
+
+require 'rails_helper'
+describe 'タスク管理機能', type: :system do
+  describe '任意の詳細画面遷移' do
+    context 'タスクの内容表示' do
+      it  '該当タスクの内容が表示される' do
+        visit  task.id show_task_path
+
+
+        expect(task.id) to have_content detail
       end
     end
   end
