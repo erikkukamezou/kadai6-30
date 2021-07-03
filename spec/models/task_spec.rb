7bfdee179825a253f9c7a925bcc41c0bcd87a6e7
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-07-03 14:50:44 +0000

    修正しました

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
-describe 'タスクモデル機能', type: :model do
-  describe 'バリデーションのテスト' do
-    context 'タスクのタイトルが空の場合' do
-      it 'バリデーションにひっかる' do
-        task = Task.new(name: '', detail: '失敗テスト')
-        expect(task).not_to be_valid
+# RSpec.describe Task, type: :model do
+  describe 'タスクモデル機能', type: :model do
+    describe 'バリデーションのテスト' do
+      context 'タスクの名前が空の場合' do
+        it 'バリデーションにひっかる' do
+          task = Task.new(name: '', detail: '失敗テスト2')
+          expect(task).not_to be_valid
+        end
       end
-    end
-　　context 'タスクの詳細が空の場合' do
-      it 'バリデーションにひっかかる' do
-        # ここに内容を記載する
-        task =Task.new(name:'失敗テスト', detail:'')
-        expect(task).not_to be_valid
+  　　context 'タスクの詳細が空の場合' do
+        it 'バリデーションにひっかかる' do
+          # ここに内容を記載する
+          task = Task.new(name:'失敗テスト1', detail:'')
+          expect(task).not_to be_valid
+        end
       end
-    end
-    context 'タスクのタイトルと詳細に内容が記載されている場合' do
-      it 'バリデーションが通る' do
-        # ここに内容を記載する
-        task = Task.new(name: '失敗テスト', detail: '失敗テスト')
-        expect(tack) to pass
+      context 'タスクの名前と詳細に内容が記載されている場合' do
+        it 'バリデーションが通る' do
+          # ここに内容を記載する
+          task = Task.new(name: '失敗テスト1', detail: '失敗テスト2')
+          expect(tack).to be_valid
+        end
       end
     end
   end
+# end
